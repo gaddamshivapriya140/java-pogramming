9ed5e634e206759230ded4ce088305ec36f7be78
gaddamshivapriya140 <gaddamshivapriya140@gmail.com>
2025-01-11 19:05:47 +0530

    Add files via upload

---
+++ ps002/NextMultipleOf100.java
@@ -0,0 +1,16 @@
+package ps002;
+import java.util.Scanner;
+public class NextMultipleOf100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number <= 0) {
+            System.out.println(-1);
+        } else {
+            int nextMultiple = ((number / 100) + 1) * 100;
+            System.out.println("Next multiple of 100: " + nextMultiple);
+        }
+        sc.close();
+  }
+}
